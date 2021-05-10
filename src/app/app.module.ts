5d01a3bf3680d2efc592c225d7f77412dde8ef00
bln23 <belen@nextail.co>
2021-05-10 09:37:59 +0200

    firts version angular proyect

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SliderComponent } from './slider/slider.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { FilmsComponent } from './films/films.component';
+import { PageComponent } from './page/page.component';
+import { HomeComponent } from './home/home.component';
+import { FormComponent } from './form/form.component';
+import { ErrorComponent } from './error/error.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SliderComponent,
+    SidebarComponent,
+    FilmsComponent,
+    PageComponent,
+    HomeComponent,
+    FormComponent,
+    ErrorComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
